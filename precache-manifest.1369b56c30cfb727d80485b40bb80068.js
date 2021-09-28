8dd22e485175bfb8fc68e1613007585ddddd51f1
Guillaume Richard <lord0gnome@gmail.com>
2021-09-28 15:31:50 +0200

    Updates

---
+++ precache-manifest.1369b56c30cfb727d80485b40bb80068.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73228ac32a49bd5ece7cab02fa4f75e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7eb550637c588b7bc8",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "3009fe0308bc92176f51",
+    "url": "/static/css/main.f4e48f44.chunk.css"
+  },
+  {
+    "revision": "9b7eb550637c588b7bc8",
+    "url": "/static/js/2.846d4e4b.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.846d4e4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3009fe0308bc92176f51",
+    "url": "/static/js/main.8aaa347c.chunk.js"
+  },
+  {
+    "revision": "b4d8166cc4e14de925b0",
+    "url": "/static/js/runtime-main.902e369a.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/static/media/laptop1.87561258.jpg"
+  }
+]);
